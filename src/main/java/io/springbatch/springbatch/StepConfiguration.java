7925e49bcbeb889c6d0b7298255db579cc71c946
mygoaway <mygoaway2@gmail.com>
2022-07-24 23:12:15 +0900

    JobLauncher 관련 커밋

--- src/main/java/io/springbatch/springbatch/StepConfiguration.java
+++ src/main/java/io/springbatch/springbatch/StepConfiguration.java
@@ -2,7 +2,6 @@ package io.springbatch.springbatch;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.Job;
-import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
@@ -13,27 +12,23 @@ import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Date;
-import java.util.Map;
-
-// @Configuration
+@Configuration
 @RequiredArgsConstructor
-public class JobExecutionConfiguration {
+public class StepConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
 
-    /*
     @Bean
     public Job job() {
-        return jobBuilderFactory.get("jobExecution")
-                .start(jobInstanceStep1())
-                .next(jobInstanceStep2())
+        return jobBuilderFactory.get("Job")
+                .start(step1())
+                .next(step2())
                 .build();
     }
 
     @Bean
-    public Step jobInstanceStep1() {
-        return stepBuilderFactory.get("jobInstanceStep1")
+    public Step step1() {
+        return stepBuilderFactory.get("step1")
                 .tasklet(new Tasklet() {
                     @Override
                     public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
@@ -46,18 +41,9 @@ public class JobExecutionConfiguration {
     }
 
     @Bean
-    public Step jobInstanceStep2() {
-        return stepBuilderFactory.get("jobInstanceStep2")
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("step2 was executed");
-                        // throw new RuntimeException("step2 has failed");
-                        return RepeatStatus.FINISHED;
-                    }
-                })
+    public Step step2() {
+        return stepBuilderFactory.get("step2")
+                .tasklet(new CustomTasklet())
                 .build();
     }
-
-     */
 }

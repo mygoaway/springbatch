6080b0d30ed134d6a5f13824ab3b590a4eb569a8
mygoaway <mygoaway2@gmail.com>
2022-08-06 15:24:55 +0900

    스프링 배치 실행 - Job 강의 끝

--- src/main/java/io/springbatch/springbatch/SimpleJobConfiguration.java
+++ src/main/java/io/springbatch/springbatch/SimpleJobConfiguration.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.batch.core.*;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.DefaultJobParametersValidator;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.context.annotation.Bean;
@@ -21,25 +22,10 @@ public class SimpleJobConfiguration {
                 .start(step1())
                 .next(step2())
                 .next(step3())
-                .incrementer(new RunIdIncrementer())
-                .validator(new JobParametersValidator() {
-                    @Override
-                    public void validate(JobParameters parameters) throws JobParametersInvalidException {
-
-                    }
-                })
-                .preventRestart()
-                .listener(new JobExecutionListener() {
-                    @Override
-                    public void beforeJob(JobExecution jobExecution) {
-
-                    }
-
-                    @Override
-                    public void afterJob(JobExecution jobExecution) {
-
-                    }
-                })
+                //.validator(new CustomJobParametersValidator()) // 검증에서 실패하면 Job 실행 자체가 안됨(Batch 관련 DB 저장안됨)
+                //.validator(new DefaultJobParametersValidator(new String[]{"name", "date"}, new String[]{"count"}))
+                //.preventRestart() // 재시작을 하지 않음 (restartable = false)
+                .incrementer(new CustomJobParametersIncrement()) // run.id 값은 계속 변하기 때문에 동일한 파라미터를 넘겨주더라도 항상 배치가 실행된다.
                 .build(); // SimpleJob 객체 생성(step으로만 구성이 가능하다.), SimpleJob 클래스는 Job 인터페이스의 자식
     }
 
@@ -68,8 +54,6 @@ public class SimpleJobConfiguration {
     public Step step3() {
         return stepBuilderFactory.get("step3")
                 .tasklet((contribution, chunkContext) -> {
-                    chunkContext.getStepContext().getStepExecution().setStatus(BatchStatus.FAILED);
-                    contribution.setExitStatus(ExitStatus.STOPPED);
                     System.out.println("step3 was executed");
                     return RepeatStatus.FINISHED;
                 })
